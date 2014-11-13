461fcbfa615ef321f8f17eaabd53d6d2507ff9ca
keertip <keertip@google.com>
2014-11-12 19:26:30 -0800

    adress comments

--- bin/dartdoc.dart
+++ bin/dartdoc.dart
@@ -8,14 +8,11 @@ import 'package:args/args.dart';
 
 import 'package:dartdoc/dartdoc.dart';
 
-
-/**
- * Analyzes Dart files and generates a representation of included libraries,
- * classes, and members. Uses the current directory to look for libraries.
- */
+/// Analyzes Dart files and generates a representation of included libraries,
+/// classes, and members. Uses the current directory to look for libraries.
 void main(List<String> arguments) {
-  ArgParser parser = _createArgsParser();
-  ArgResults results = parser.parse(arguments);
+  var parser = _createArgsParser();
+  var results = parser.parse(arguments);
 
   if (results['help']) {
     _printUsageAndExit(parser);
@@ -23,23 +20,20 @@ void main(List<String> arguments) {
   List<String> excludeLibraries = results['exclude'] == null ?
          [] : results['exclude'].split(',');
 
-  Directory currentDir = Directory.current;
+  var currentDir = Directory.current;
   new DartDoc(currentDir, excludeLibraries).generate();
 }
 
-/**
- * Print help if we are passed the help option or invalid arguments.
- */
+/// Print help if we are passed the help option or invalid arguments.
 void _printUsageAndExit(ArgParser parser) {
   print(parser.usage);
   print('Usage: dartdoc [OPTIONS]');
   exit(0);
 }
 
-
  ArgParser _createArgsParser() {
    // TODO: more options to be added
-  ArgParser parser = new ArgParser();
+  var parser = new ArgParser();
   parser.addOption(
       'exclude',
       help: 'a comma-separated list of library names to ignore');
--- lib/dartdoc.dart
+++ lib/dartdoc.dart
@@ -2,29 +2,22 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
 library dartdoc;
 
 import 'dart:io';
 
-/**
- * Generates Dart documentation for all public Dart libraries in the given
- * directory.
- */
+/// Generates Dart documentation for all public Dart libraries in the given
+/// directory.
 class DartDoc {
 
   List<String> _excludes;
   Directory _rootDir;
 
-
   DartDoc(this._rootDir,this._excludes);
 
-  /**
-   * Generate the documentation
-   */
+  /// Generate the documentation
   void generate() {
     print("TODO: impelement doc generation");
   }
-
 }
 
