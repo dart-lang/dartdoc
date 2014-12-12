53212081d6376dd4d7e8f0f68216923f553735bf
keertip <keertip@google.com>
2014-12-12 12:57:18 -0800

    add test for url

--- test/template_test.dart
+++ test/template_test.dart
@@ -45,5 +45,18 @@ tests() {
 ''');
       });
     
+      test('url and file name', () {
+           expect(sitemap({'url': 'http://mydoc.com','links' : [{'name': 'somefile.html'}]}), 
+                 '''
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+  <url>
+    <loc>http://mydoc.com/somefile.html</loc>
+  </url>
+</urlset>
+''');
+           });
+      
+      
     });
 }
