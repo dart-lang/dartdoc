d24f86e5c63cec0c25825c92699aa76ee3f92f77
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-02-15 15:01:24 +0100

    use dart support for travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,25 +7,9 @@
 # Fast fail the script on failures.
 set -e
 
-# Get the Dart SDK (only for travis-ci.org; otherwise, assume that Dart is already available).
-if [ "$TRAVIS" = "true" ]; then
-  DART_DIST=dartsdk-linux-x64-release.zip
-  curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
-  unzip $DART_DIST > /dev/null
-  rm $DART_DIST
-  export DART_SDK="$PWD/dart-sdk"
-  export PATH="$DART_SDK/bin:$PATH"
-fi
-
-# Display installed versions.
-dart --version
-
 # Globally install grinder.
 pub global activate grinder
 export PATH="$PATH":"~/.pub-cache/bin"
-
-# Get our packages.
-pub get
 
 # Verify that the libraries are error free.
 # uncomment when MockClass is no longer needed
