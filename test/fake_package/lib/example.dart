b25ec93bee8a0f88e4455742278a57f623caad62
keertip <keertip@google.com>
2015-04-13 15:54:28 -0700

    fix whitespace

--- test/fake_package/lib/example.dart
+++ test/fake_package/lib/example.dart
@@ -9,7 +9,7 @@ double number;
 
 get y => 2;
 
-const String COLOR='red';
+const String COLOR = 'red';
 const String COLOR_GREEN = 'green';
 const String COLOR_ORANGE = 'orange';
 const String COMPLEX_COLOR = 'red' + '-' + 'green' + '-' + 'blue';
