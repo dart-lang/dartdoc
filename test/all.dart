d0d183ae4778e587425edec13fa324a2417264fd
Devon Carew <devoncarew@gmail.com>
2015-05-21 13:11:18 -0700

    remove HtmlPrinter

--- test/all.dart
+++ test/all.dart
@@ -6,7 +6,6 @@ library dartdoc.all_tests;
 
 import 'dartdoc_test.dart' as dartdoc_tests;
 import 'html_generator_test.dart' as html_generator_tests;
-import 'html_printer_test.dart' as html_printer_test;
 import 'io_utils_test.dart' as io_utils_tests;
 import 'model_test.dart' as model_tests;
 import 'package_meta_test.dart' as package_meta_tests;
@@ -15,7 +14,6 @@ import 'template_test.dart' as template_tests;
 void main() {
   dartdoc_tests.main();
   html_generator_tests.main();
-  html_printer_test.main();
   io_utils_tests.main();
   model_tests.main();
   package_meta_tests.main();
