a82ec7baa724b8575b0896896e6b53fab13dd14a
Devon Carew <devoncarew@gmail.com>
2015-05-29 14:29:55 -0700

    tabs to spaces

--- tool/travis.sh
+++ tool/travis.sh
@@ -11,40 +11,39 @@ set -e
 pub global activate grinder
 export PATH="$PATH":"~/.pub-cache/bin"
 
-
 if [ "$GEN_SDK_DOCS" = "true" ]
 then
-	# Build the SDK docs
-	# silence stdout but echo stderr
-	echo ""
-	echo "Building SDK docs..."
-	grind build-sdk-docs 2>&1 >/dev/null | echo
-	echo "SDK docs process finished"
+ # Build the SDK docs
+  # silence stdout but echo stderr
+  echo ""
+  echo "Building SDK docs..."
+  grind build-sdk-docs 2>&1 >/dev/null | echo
+  echo "SDK docs process finished"
 else
-	echo ""
-    echo "Skipping SDK docs, because GEN_SDK_DOCS is $GEN_SDK_DOCS"
-    echo ""
+  echo ""
+  echo "Skipping SDK docs, because GEN_SDK_DOCS is $GEN_SDK_DOCS"
+  echo ""
 
-    $(dirname -- "$0")/ensure_dartfmt.sh
+  $(dirname -- "$0")/ensure_dartfmt.sh
 
-    # Verify that the libraries are error free.
-    grind analyze
+  # Verify that the libraries are error free.
+  grind analyze
 
-    # Another smoke test: Run dartdoc on test_package.
-	cd test_package
-	dart -c ../bin/dartdoc.dart
-	cd ..
+  # Another smoke test: Run dartdoc on test_package.
+  cd test_package
+  dart -c ../bin/dartdoc.dart
+  cd ..
 
-	# Run the tests.
-	grind test
+  # Run the tests.
+  grind test
 
-	# Gather and send coverage data.
-	if [ "$REPO_TOKEN" ]; then
-	  pub global activate dart_coveralls
-	  pub global run dart_coveralls report \
-	    --token $REPO_TOKEN \
-	    --retry 2 \
-	    --exclude-test-files \
-	    test/all.dart
-	fi
+  # Gather and send coverage data.
+  if [ "$REPO_TOKEN" ]; then
+    pub global activate dart_coveralls
+    pub global run dart_coveralls report \
+      --token $REPO_TOKEN \
+      --retry 2 \
+      --exclude-test-files \
+      test/all.dart
+  fi
 fi
