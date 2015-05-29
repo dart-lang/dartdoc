af748e7d4216055a3b4b6619ff6aeeb80b01347e
keertip <keertip@google.com>
2015-05-29 09:43:05 -0700

    fix for finding library for element

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -195,13 +195,18 @@ abstract class ModelElement {
         return null;
       }
       var refLibrary;
-      if (this is Library) {
-        refLibrary = this;
-      } else {
-        refLibrary = _getLibraryFor(library._library, this.package);
+      try {
+        var element = refElement is ClassMemberElement
+            ? refElement.enclosingElement
+            : refElement;
+        refLibrary = this.package._libraries
+            .firstWhere((lib) => lib.hasInNamespace(element));
+      } on StateError {}
+      if (refLibrary != null) {
+        var e = new ModelElement.from(refElement, refLibrary);
+        return e.href;
       }
-      var e = new ModelElement.from(refElement, refLibrary);
-      return e.href;
+      return null;
     }
 
     return replaceAllLinks(docs, _getMatchingLink);
@@ -494,8 +499,8 @@ class Library extends ModelElement {
   List<ModelFunction> _functions;
   List<Typedef> _typeDefs;
   List<TopLevelVariable> _variables;
-  Iterable<Element> _nameSpace;
-
+  Iterable<Element> _nameSpaceElements;
+  Namespace _namespace;
   String _name;
 
   LibraryElement get _library => (element as LibraryElement);
@@ -504,15 +509,21 @@ class Library extends ModelElement {
 
   Library get library => this;
 
-  Iterable<Element> get _exportedNameSpace {
-    if (_nameSpace == null) _buildExportedNameSpace();
-    return _nameSpace;
+  Iterable<Element> get _exportedNamespace {
+    if (_nameSpaceElements == null) _buildExportedNamespace();
+    return _nameSpaceElements;
   }
 
-  _buildExportedNameSpace() {
-    Namespace namespace =
+  _buildExportedNamespace() {
+    _namespace =
         new NamespaceBuilder().createExportNamespaceForLibrary(_library);
-    _nameSpace = namespace.definedNames.values;
+    _nameSpaceElements = _namespace.definedNames.values;
+  }
+
+  bool hasInNamespace(Element element) {
+    if (_namespace == null) _buildExportedNamespace();
+    var e = _namespace.get(element.name);
+    return e == element;
   }
 
   String get name {
@@ -547,7 +558,7 @@ class Library extends ModelElement {
     for (CompilationUnitElement cu in _library.parts) {
       elements.addAll(cu.topLevelVariables);
     }
-    _exportedNameSpace.forEach((element) {
+    _exportedNamespace.forEach((element) {
       if (element is PropertyAccessorElement) elements.add(element.variable);
     });
     elements..removeWhere(isPrivate);
@@ -577,7 +588,7 @@ class Library extends ModelElement {
     if (_enums != null) return _enums;
 
     List<ClassElement> enumClasses = [];
-    enumClasses.addAll(_exportedNameSpace
+    enumClasses.addAll(_exportedNamespace
         .where((element) => element is ClassElement && element.isEnum));
     _enums = enumClasses
         .where(isPublic)
@@ -601,7 +612,7 @@ class Library extends ModelElement {
       elements.addAll(cu.functionTypeAliases);
     }
 
-    elements.addAll(_exportedNameSpace
+    elements.addAll(_exportedNamespace
         .where((element) => element is FunctionTypeAliasElement));
     elements..removeWhere(isPrivate);
     _typeDefs = elements.map((e) => new Typedef(e, this)).toList();
@@ -619,7 +630,7 @@ class Library extends ModelElement {
       elements.addAll(cu.functions);
     }
     elements.addAll(
-        _exportedNameSpace.where((element) => element is FunctionElement));
+        _exportedNamespace.where((element) => element is FunctionElement));
 
     elements..removeWhere(isPrivate);
     _functions = elements.map((e) {
@@ -638,11 +649,11 @@ class Library extends ModelElement {
     }
     for (LibraryElement le in _library.exportedLibraries) {
       types.addAll(le.definingCompilationUnit.types
-          .where((t) => _exportedNameSpace.contains(t.name))
+          .where((t) => _exportedNamespace.contains(t.name))
           .toList());
     }
 
-    types.addAll(_exportedNameSpace
+    types.addAll(_exportedNamespace
         .where((element) => element is ClassElement && !element.isEnum));
 
     _classes = types
