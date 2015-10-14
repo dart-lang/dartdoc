5e04149ca6a668479b5aeb8efc66fb57330bbab7
Anton Astashov <anton.astashov@gmail.com>
2015-10-14 11:38:37 -0500

    Add redirector script from the old dartdocs.org path schema
    The old schema was something like
    index.html#package/library.Class@id_method. So, we parse it and try to
    redirect to the new dartdoc's path. For that, we first make a HEAD
    request to check if that location actually exists, and if yes - change
    location.href to that new location
    
    It doesn't always work, because there are some changes between the old
    schema and the new schema, so sometimes the redirect would lead to 404.
    Known cases:
    
    * Operators. Old dartdocs.org doesn't make a distinction between
      an operator and e.g. a method, but the new dartdoc does. So, there is
      no way to know if this is an operator or not, so the redirect won't
      work.
    
    * Inherited methods. If class B extends A, and B has inherited methods
      from A, then dartdocs.org will show them as #package/lib.B@id_foo, and
      dartdoc will consider it as lib/A/foo.html, so the redirect won't work
      either.
    
    There may be other cases, but most of the time it should work fine.

--- lib/src/resources.g.dart
+++ lib/src/resources.g.dart
@@ -1,4 +1,4 @@
-// WARNING: This file is auto-generated. Do not taunt.
+// WARNING: This file is auto-generated.
 
 library resources;
 
@@ -6,6 +6,7 @@ const List<String> resource_names = const [
   'package:dartdoc/resources/favicon.png',
   'package:dartdoc/resources/prettify.css',
   'package:dartdoc/resources/prettify.js',
+  'package:dartdoc/resources/redirector.js',
   'package:dartdoc/resources/script.js',
   'package:dartdoc/resources/styles.css',
   'package:dartdoc/resources/typeahead.bundle.min.js',
