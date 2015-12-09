8529eda1c1bf73b60c8a91d9504601c66ca024d5
keertip <keertip@google.com>
2015-12-09 13:45:03 -0800

    run tests only once

--- tool/travis.sh
+++ tool/travis.sh
@@ -46,16 +46,17 @@ else
   dart -c ../bin/dartdoc.dart
   cd ..
 
-  # Run the tests.
-  grind test
-
   # Gather and send coverage data.
-  if [ "$REPO_TOKEN" ]; then
+  if [ "$REPO_TOKEN" ]
+  then
     pub global activate dart_coveralls
     pub global run dart_coveralls report \
       --token $REPO_TOKEN \
       --retry 2 \
       --exclude-test-files \
       test/all.dart
+  else
+    # Only run the tests.
+    grind test
   fi
 fi
