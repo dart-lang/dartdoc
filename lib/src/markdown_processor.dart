8fe455d95ae620804e5b3aafd1ac5b6bfb5956ca
Devon Carew <devoncarew@google.com>
2016-12-27 10:47:47 -0800

    small tweak to the qualified reference resolution code (#1309)
    * small tweak to the qualified reference resolution code
    
    * typo

--- lib/src/markdown_processor.dart
+++ lib/src/markdown_processor.dart
@@ -123,9 +123,12 @@ MatchingLinkResult _findRefElementInLibrary(String codeRef, ModelElement element
     }
   }
 
-  for (final modelElement in library.allModelElements) {
-    if (codeRef == modelElement.fullyQualifiedNameWithoutLibrary) {
-      result[modelElement.fullyQualifiedName] = modelElement;
+  // Only look for partially qualified matches if we didn't find a fully qualified one.
+  if (result.isEmpty) {
+    for (final modelElement in library.allModelElements) {
+      if (codeRef == modelElement.fullyQualifiedNameWithoutLibrary) {
+        result[modelElement.fullyQualifiedName] = modelElement;
+      }
     }
   }
 
