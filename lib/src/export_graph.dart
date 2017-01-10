7478a4f3d90f6e2d4f63db28e5f3cbf9040626d1
Anton Astashov <anton.astashov@gmail.com>
2017-01-10 14:23:01 -0600

    Deemphasize reexported symbols [#1158] (#1319)
    * Deemphasize reexported symbols [#1158]
    
    @Hixie noticed, that when a library exports another library, but both
    libraries are being docced together, the result is that each class that
    is exported by both libraries is listed twice.
    
    We should still list classes and other symbols in the libraries that
    reexport them (since they still part of public API of those libs), but
    we only gonna show "canonical" symbols in Search, and also we won't
    create duplicate HTML pages for reexported symbols.
    
    We define the reexported symbols in the following way:
    
    1. A defines Foo.
    2. Foo doesn't appear in the docs for A because A is a private library,
       i.e., in lib/src/../a.dart
    3. B exports Foo from A.
    4. Foo appears in the docs for B.
    5. C exports Foo from B.
    6. Foo appears in the docs for C.
    
    So, Foo of lib C shouldn't have its own page and also shouldn't appear
    in Search, because Foo was exported from B and B already has docs. All
    links to Foo from HTML docs of C should point to the Foo of B.
    
    To solve that, we build directed "export graph", where we track what
    libs export what libs. Then, to find the "canonical" lib, we go up the
    graph until we find first documented lib - that's going to be the
    canonical one.
    
    Then, we filter out non-canonical libs when building the search index,
    and also use canonical lib in `.href` getters, this way ensuring we
    always link to symbols in canonical libs and don't create HTML files for
    symbols from non-canonical libs.
    
    Testing: Unit tests, also built test docs (`testing/test_package_docs`),
    made sure we don't create files for non-canonical stuff (i.e. we create
    `fake/Cool` files, but not `ex/Cool`). Also, built Flutter docs, ensured
    we don't create `material/BoxDecoration`, `rendering/BoxDecoration` and
    `widgets/BoxDecoration`, and only create `painting/BoxDecoration` files,
    and all the links point to `painting/BoxDecoration`.
    
    * Add some docs

---
+++ lib/src/export_graph.dart
@@ -0,0 +1,70 @@
+import 'package:analyzer/dart/element/element.dart';
+
+/// Node in the directed export graph.
+///
+/// It wraps a library element, and also contains links to the nodes/libs, which export this lib,
+/// and nodes/libs, which are exported by this lib.
+class _ExportGraphNode {
+  final LibraryElement libraryElement;
+  Set<_ExportGraphNode> exportedBy = new Set();
+  Set<_ExportGraphNode> exports = new Set();
+
+  _ExportGraphNode(this.libraryElement);
+
+  /// It returns a "canonical" library element
+  ///
+  /// That's one of the passed in the arguments, which is the closest if we go up the graph.
+  _ExportGraphNode canonicalLibraryElement(Iterable<LibraryElement> libraryElements) {
+    if (libraryElements.contains(libraryElement)) {
+      return this;
+    } else {
+      return exportedBy.toList().firstWhere((l) => l.canonicalLibraryElement(libraryElements) != null);
+    }
+  }
+}
+
+/// Recursively builds the export graph, and also populates the index [map]. It takes
+/// the library element from the arguments, adds it to the index, then goes through all
+/// the libraries this library element exports, adds them to the index too and also connects
+/// them to build a graph.
+void _buildSubGraph(Map<String, _ExportGraphNode> map, LibraryElement libraryElement) {
+  if (!map.containsKey(libraryElement.source.fullName)) {
+    map[libraryElement.source.fullName] = new _ExportGraphNode(libraryElement);
+  }
+  final node = map[libraryElement.source.fullName];
+  libraryElement.exports.forEach((ExportElement export) {
+    final exportedLibraryElement = export.exportedLibrary;
+    if (!map.containsKey(exportedLibraryElement.source.fullName)) {
+      map[exportedLibraryElement.source.fullName] = new _ExportGraphNode(exportedLibraryElement);
+    }
+    final childNode = map[exportedLibraryElement.source.fullName];
+    childNode.exportedBy.add(node);
+    node.exports.add(childNode);
+    _buildSubGraph(map, exportedLibraryElement);
+  });
+}
+
+/// Directed graph, which allows to track what libs export and being exported by what libs
+/// (where libs are the documentable package libraries).
+///
+/// Also, allows to find the "canonical" library element for the provided element, i.e. the
+/// one, which should contain documentation of the provided element.
+class ExportGraph {
+  final Map<String, _ExportGraphNode> map = {};
+  final Iterable<LibraryElement> packageLibraryElements;
+
+  ExportGraph(this.packageLibraryElements) {
+    packageLibraryElements.forEach((LibraryElement libraryElement) {
+      _buildSubGraph(map, libraryElement);
+    });
+  }
+
+  LibraryElement canonicalLibraryElement(Element element) {
+    if (map.containsKey(element.library.source.fullName)) {
+      final node = map[element.library.source.fullName];
+      return node.canonicalLibraryElement(packageLibraryElements)?.libraryElement;
+    } else {
+      return element.library;
+    }
+  }
+}
