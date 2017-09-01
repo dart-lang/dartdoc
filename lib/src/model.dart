39c22c5b50821db6ca0031f2d661a7fe2f7e2c04
jcollins-g <jcollins@google.com>
2017-09-01 12:09:31 -0700

    Allow null annotation elements (#1493)
    * Allow annotations to not have a resolved element.
    
    * Revert errant model.dart change

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -2332,7 +2332,8 @@ abstract class ModelElement extends Nameable
     if (md == null) md = new List<dynamic>();
     return md.map((dynamic a) {
       String annotation = (const HtmlEscape()).convert(a.toSource());
-      var me = package.findCanonicalModelElementFor(a.element.enclosingElement);
+      // a.element can be null if the element can't be resolved.
+      var me = package.findCanonicalModelElementFor(a.element?.enclosingElement);
       if (me != null)
         annotation = annotation.replaceFirst(me.name, me.linkedName);
       return annotation;
