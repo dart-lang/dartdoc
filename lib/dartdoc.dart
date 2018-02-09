45c38a6701c861f9d2d680c3fdee514a8391a8d5
Janice Collins <jcollins@google.com>
2018-02-09 10:59:51 -0800

    0.16.0 and new analyzer

--- lib/dartdoc.dart
+++ lib/dartdoc.dart
@@ -35,7 +35,7 @@ export 'src/sdk.dart';
 
 const String name = 'dartdoc';
 // Update when pubspec version changes.
-const String version = '0.15.1';
+const String version = '0.16.0';
 
 final String defaultOutDir = path.join('doc', 'api');
 
