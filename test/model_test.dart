b1b455af9fd1ec14179ad21411a18076e7e71500
Greg Spencer <gspencer@google.com>
2018-06-07 10:37:17 -0700

    Fix analyzer warning in test

--- test/model_test.dart
+++ test/model_test.dart
@@ -576,8 +576,8 @@ void main() {
           isTrue);
     });
     test("Doesn't place animations in one line doc", () {
-      expect(withAnimationInline.oneLineDoc, isNot(contains('<video')));
-      expect(withAnimationInline.documentation, contains('<video'));
+      expect(withAnimationInOneLineDoc.oneLineDoc, isNot(contains('<video')));
+      expect(withAnimationInOneLineDoc.documentation, contains('<video'));
     });
     test("Handles animations inline properly", () {
       expect(withAnimationInline.documentation, isNot(contains('  works')));
