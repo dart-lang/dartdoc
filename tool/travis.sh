27369f2847a446859c668929925dd1da91db764c
Janice Collins <jcollins@google.com>
2018-07-23 15:44:54 -0700

    Fix CI typos

--- tool/travis.sh
+++ tool/travis.sh
@@ -22,7 +22,7 @@ elif [ "$DARTDOC_BOT" = "flutter" ]; then
   pub run grinder validate-flutter-docs
 elif [ "$DARTDOC_BOT" = "packages" ]; then
   echo "Running packages dartdoc bot"
-  PACKAGE_NAME=angular PACKAGE_VERSION=">=5.0.0-beta" DARTDOC_PARAMS="--include=angular,angular.security" grind build-pub-package
+  PACKAGE_NAME=angular PACKAGE_VERSION=">=5.0.0-beta" DARTDOC_PARAMS="--include=angular,angular.security" pub run grinder build-pub-package
 else
   echo "Running main dartdoc bot"
   pub run grinder buildbot
