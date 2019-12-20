d6e8c3b214a8cb0b0560834dc6cfeb1bd5d06bbd
Jonathan Koren <jdkoren@google.com>
2019-12-20 11:13:46 -0800

    Start html generator backend

--- lib/src/generator_fe.dart
+++ lib/src/generator_fe.dart
@@ -9,6 +9,7 @@ import 'package:dartdoc/src/generator.dart';
 import 'package:dartdoc/src/logging.dart';
 import 'package:dartdoc/src/model/model.dart';
 import 'package:dartdoc/src/model_utils.dart';
+import 'package:dartdoc/src/warnings.dart';
 import 'package:path/path.dart' as path;
 
 abstract class FileWriter {
@@ -45,8 +46,8 @@ class GeneratorFrontEnd implements Generator, FileWriter {
     _generateDocs(packageGraph, indexElements);
     _generatorBackend.generateAdditionalFiles(this, packageGraph);
 
-    Iterable<Categorization> categories = indexElements
-        .where((e) => e is Categorization && e.hasCategorization);
+    Iterable<Categorization> categories =
+        indexElements.where((e) => e is Categorization && e.hasCategorization);
     _generatorBackend.generateCategoryJson(this, categories);
     _generatorBackend.generateSearchIndex(this, indexElements);
   }
@@ -56,7 +57,8 @@ class GeneratorFrontEnd implements Generator, FileWriter {
     if (packageGraph == null) return;
 
     logInfo('documenting ${packageGraph.defaultPackage.name}');
-    _generatorBackend.generatePackage(this, packageGraph, packageGraph.defaultPackage);
+    _generatorBackend.generatePackage(
+        this, packageGraph, packageGraph.defaultPackage);
 
     for (var package in packageGraph.localPackages) {
       for (var category in filterNonDocumented(package.categories)) {
@@ -69,6 +71,9 @@ class GeneratorFrontEnd implements Generator, FileWriter {
       for (var lib in filterNonDocumented(package.libraries)) {
         logInfo('Generating docs for library ${lib.name} from '
             '${lib.element.source.uri}...');
+        if (!lib.isAnonymous && !lib.hasDocumentation) {
+          packageGraph.warnOnElement(lib, PackageWarning.noLibraryLevelDocs);
+        }
         indexAccumulator.add(lib);
         _generatorBackend.generateLibrary(this, packageGraph, lib);
 
@@ -80,92 +85,101 @@ class GeneratorFrontEnd implements Generator, FileWriter {
             if (!constructor.isCanonical) continue;
 
             indexAccumulator.add(constructor);
-            _generatorBackend.generateConstructor(this, packageGraph, lib, clazz, constructor);
+            _generatorBackend.generateConstructor(
+                this, packageGraph, lib, clazz, constructor);
           }
 
           for (var constant in filterNonDocumented(clazz.constants)) {
             if (!constant.isCanonical) continue;
 
             indexAccumulator.add(constant);
-            _generatorBackend.generateConstant(this, packageGraph, lib, clazz, constant);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, clazz, constant);
           }
 
           for (var property in filterNonDocumented(clazz.staticProperties)) {
             if (!property.isCanonical) continue;
 
             indexAccumulator.add(property);
-            _generatorBackend.generateProperty(this, packageGraph, lib, clazz, property);
+            _generatorBackend.generateProperty(
+                this, packageGraph, lib, clazz, property);
           }
 
           for (var property in filterNonDocumented(clazz.allInstanceFields)) {
             if (!property.isCanonical) continue;
 
             indexAccumulator.add(property);
-            _generatorBackend.generateProperty(this, packageGraph, lib, clazz, property);
+            _generatorBackend.generateProperty(
+                this, packageGraph, lib, clazz, property);
           }
 
           for (var method in filterNonDocumented(clazz.allInstanceMethods)) {
             if (!method.isCanonical) continue;
 
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this, packageGraph, lib, clazz, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, clazz, method);
           }
 
           for (var operator in filterNonDocumented(clazz.allOperators)) {
             if (!operator.isCanonical) continue;
 
             indexAccumulator.add(operator);
-            _generatorBackend.generateMethod(this, packageGraph, lib, clazz, operator);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, clazz, operator);
           }
 
           for (var method in filterNonDocumented(clazz.staticMethods)) {
             if (!method.isCanonical) continue;
 
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this, packageGraph, lib, clazz, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, clazz, method);
           }
         }
 
         for (var extension in filterNonDocumented(lib.extensions)) {
           indexAccumulator.add(extension);
-          _generatorBackend.generateExtension(this, packageGraph, lib, extension);
+          _generatorBackend.generateExtension(
+              this, packageGraph, lib, extension);
 
           for (var constant in filterNonDocumented(extension.constants)) {
             indexAccumulator.add(constant);
-            _generatorBackend.generateConstant(this, packageGraph, lib, extension, constant);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, extension, constant);
           }
 
           for (var property
               in filterNonDocumented(extension.staticProperties)) {
             indexAccumulator.add(property);
-            _generatorBackend.generateProperty(this,
-                    packageGraph, lib, extension, property);
+            _generatorBackend.generateProperty(
+                this, packageGraph, lib, extension, property);
           }
 
           for (var method
               in filterNonDocumented(extension.allPublicInstanceMethods)) {
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this,
-                    packageGraph, lib, extension, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, extension, method);
           }
 
           for (var method in filterNonDocumented(extension.staticMethods)) {
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this,
-                    packageGraph, lib, extension, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, extension, method);
           }
 
           for (var operator in filterNonDocumented(extension.allOperators)) {
             indexAccumulator.add(operator);
-            _generatorBackend.generateMethod(this,
-                    packageGraph, lib, extension, operator);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, extension, operator);
           }
 
           for (var property
               in filterNonDocumented(extension.allInstanceFields)) {
             indexAccumulator.add(property);
-            _generatorBackend.generateProperty(this,
-                    packageGraph, lib, extension, property);
+            _generatorBackend.generateProperty(
+                this, packageGraph, lib, extension, property);
           }
         }
 
@@ -177,50 +191,55 @@ class GeneratorFrontEnd implements Generator, FileWriter {
             if (!constructor.isCanonical) continue;
 
             indexAccumulator.add(constructor);
-            _generatorBackend.generateConstructor(this,
-                    packageGraph, lib, mixin, constructor);
+            _generatorBackend.generateConstructor(
+                this, packageGraph, lib, mixin, constructor);
           }
 
           for (var constant in filterNonDocumented(mixin.constants)) {
             if (!constant.isCanonical) continue;
             indexAccumulator.add(constant);
-            _generatorBackend.generateConstant(this,
-                    packageGraph, lib, mixin, constant);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, mixin, constant);
           }
 
           for (var property in filterNonDocumented(mixin.staticProperties)) {
             if (!property.isCanonical) continue;
 
             indexAccumulator.add(property);
-            _generatorBackend.generateConstant(this, packageGraph, lib, mixin, property);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, mixin, property);
           }
 
           for (var property in filterNonDocumented(mixin.allInstanceFields)) {
             if (!property.isCanonical) continue;
 
             indexAccumulator.add(property);
-            _generatorBackend.generateConstant(this, packageGraph, lib, mixin, property);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, mixin, property);
           }
 
           for (var method in filterNonDocumented(mixin.allInstanceMethods)) {
             if (!method.isCanonical) continue;
 
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this, packageGraph, lib, mixin, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, mixin, method);
           }
 
           for (var operator in filterNonDocumented(mixin.allOperators)) {
             if (!operator.isCanonical) continue;
 
             indexAccumulator.add(operator);
-            _generatorBackend.generateMethod(this, packageGraph, lib, mixin, operator);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, mixin, operator);
           }
 
           for (var method in filterNonDocumented(mixin.staticMethods)) {
             if (!method.isCanonical) continue;
 
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this, packageGraph, lib, mixin, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, mixin, method);
           }
         }
 
@@ -230,26 +249,31 @@ class GeneratorFrontEnd implements Generator, FileWriter {
 
           for (var property in filterNonDocumented(eNum.allInstanceFields)) {
             indexAccumulator.add(property);
-            _generatorBackend.generateConstant(this, packageGraph, lib, eNum, property);
+            _generatorBackend.generateConstant(
+                this, packageGraph, lib, eNum, property);
           }
           for (var operator in filterNonDocumented(eNum.allOperators)) {
             indexAccumulator.add(operator);
-            _generatorBackend.generateMethod(this, packageGraph, lib, eNum, operator);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, eNum, operator);
           }
           for (var method in filterNonDocumented(eNum.allInstanceMethods)) {
             indexAccumulator.add(method);
-            _generatorBackend.generateMethod(this, packageGraph, lib, eNum, method);
+            _generatorBackend.generateMethod(
+                this, packageGraph, lib, eNum, method);
           }
         }
 
         for (var constant in filterNonDocumented(lib.constants)) {
           indexAccumulator.add(constant);
-          _generatorBackend.generateTopLevelConstant(this, packageGraph, lib, constant);
+          _generatorBackend.generateTopLevelConstant(
+              this, packageGraph, lib, constant);
         }
 
         for (var property in filterNonDocumented(lib.properties)) {
           indexAccumulator.add(property);
-          _generatorBackend.generateTopLevelProperty(this, packageGraph, lib, property);
+          _generatorBackend.generateTopLevelProperty(
+              this, packageGraph, lib, property);
         }
 
         for (var function in filterNonDocumented(lib.functions)) {
@@ -277,54 +301,59 @@ abstract class GeneratorBackend {
   void generatePackage(FileWriter writer, PackageGraph graph, Package package);
 
   /// Emit documentation content for the [category].
-  void generateCategory(FileWriter writer, PackageGraph graph, Category category);
+  void generateCategory(
+      FileWriter writer, PackageGraph graph, Category category);
 
   /// Emit documentation content for the [library].
   void generateLibrary(FileWriter writer, PackageGraph graph, Library library);
 
   /// Emit documentation content for the [clazz].
-  void generateClass(FileWriter writer, PackageGraph graph, Library library, Class clazz);
+  void generateClass(
+      FileWriter writer, PackageGraph graph, Library library, Class clazz);
 
   /// Emit documentation content for the [eNum].
-  void generateEnum(FileWriter writer, PackageGraph graph, Library library, Enum eNum);
+  void generateEnum(
+      FileWriter writer, PackageGraph graph, Library library, Enum eNum);
 
   /// Emit documentation content for the [mixin].
-  void generateMixin(FileWriter writer, PackageGraph graph, Library library, Mixin mixin);
+  void generateMixin(
+      FileWriter writer, PackageGraph graph, Library library, Mixin mixin);
 
   /// Emit documentation content for the [constructor].
-  void generateConstructor(FileWriter writer, PackageGraph graph, Library library, Class clazz,
-      Constructor constructor);
+  void generateConstructor(FileWriter writer, PackageGraph graph,
+      Library library, Class clazz, Constructor constructor);
 
   /// Emit documentation content for the [field].
-  void generateConstant(FileWriter writer,
-      PackageGraph graph, Library library, Container clazz, Field field);
+  void generateConstant(FileWriter writer, PackageGraph graph, Library library,
+      Container clazz, Field field);
 
   /// Emit documentation content for the [field].
-  void generateProperty(FileWriter writer,
-      PackageGraph graph, Library library, Container clazz, Field field);
+  void generateProperty(FileWriter writer, PackageGraph graph, Library library,
+      Container clazz, Field field);
 
   /// Emit documentation content for the [method].
-  void generateMethod(FileWriter writer,
-      PackageGraph graph, Library library, Container clazz, Method method);
+  void generateMethod(FileWriter writer, PackageGraph graph, Library library,
+      Container clazz, Method method);
 
   /// Emit documentation content for the [extension].
-  void generateExtension(FileWriter writer,
-      PackageGraph graph, Library library, Extension extension);
+  void generateExtension(FileWriter writer, PackageGraph graph, Library library,
+      Extension extension);
 
   /// Emit documentation content for the [function].
-  void generateFunction(FileWriter writer,
-      PackageGraph graph, Library library, ModelFunction function);
+  void generateFunction(FileWriter writer, PackageGraph graph, Library library,
+      ModelFunction function);
 
   /// Emit documentation content for the [constant].
-  void generateTopLevelConstant(FileWriter writer,
-      PackageGraph graph, Library library, TopLevelVariable constant);
+  void generateTopLevelConstant(FileWriter writer, PackageGraph graph,
+      Library library, TopLevelVariable constant);
 
   /// Emit documentation content for the [property].
-  void generateTopLevelProperty(FileWriter writer,
-      PackageGraph graph, Library library, TopLevelVariable property);
+  void generateTopLevelProperty(FileWriter writer, PackageGraph graph,
+      Library library, TopLevelVariable property);
 
   /// Emit documentation content for the [typedef].
-  void generateTypeDef(FileWriter writer, PackageGraph graph, Library library, Typedef typedef);
+  void generateTypeDef(
+      FileWriter writer, PackageGraph graph, Library library, Typedef typedef);
 
   /// Emit files not specific to a Dart language element.
   void generateAdditionalFiles(FileWriter writer, PackageGraph graph);
---
+++ lib/src/html/html_generator_backend.dart
@@ -0,0 +1,176 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:dartdoc/src/generator_fe.dart';
+import 'package:dartdoc/src/generator_utils.dart' as generator_util;
+import 'package:dartdoc/src/html/resource_loader.dart' as loader;
+import 'package:dartdoc/src/html/resources.g.dart' as resources;
+import 'package:dartdoc/src/html/template_data.dart';
+import 'package:dartdoc/src/html/templates.dart';
+import 'package:dartdoc/src/model/model.dart';
+import 'package:dartdoc/src/model/package.dart';
+import 'package:dartdoc/src/model/package_graph.dart';
+import 'package:mustache/mustache.dart';
+import 'package:path/path.dart' as path;
+
+class HtmlGeneratorBackend implements GeneratorBackend {
+  final HtmlOptions _options;
+  final Templates _templates;
+
+  HtmlGeneratorBackend(this._options, this._templates);
+
+  void _write(FileWriter writer, String filename, Template template,
+      TemplateData data) {
+    String content = template.renderString(data);
+    content = content.replaceAll(HTMLBASE_PLACEHOLDER, data.htmlBase);
+    writer.write(filename, content);
+  }
+
+  @override
+  void generateCategoryJson(
+      FileWriter writer, List<Categorization> categories) {
+    String json = generator_util.generateCategoryJson(categories);
+    json = json.replaceAll(HTMLBASE_PLACEHOLDER, '');
+    writer.write(path.join('categories.json'), '${json}\n');
+  }
+
+  @override
+  void generateSearchIndex(FileWriter writer, List<Indexable> indexedElements) {
+    // TODO pretty-index-json from generator options
+    String json =
+        generator_util.generateSearchIndexJson(indexedElements, false);
+    json = json.replaceAll(HTMLBASE_PLACEHOLDER, '');
+    writer.write(path.join('categories.json'), '${json}\n');
+  }
+
+  @override
+  void generatePackage(FileWriter writer, PackageGraph graph, Package package) {
+    TemplateData data = PackageTemplateData(_options, graph, package);
+    _write(writer, package.filePath, _templates.indexTemplate, data);
+    _write(writer, '__404error.html', _templates.errorTemplate, data);
+  }
+
+  @override
+  void generateCategory(
+      FileWriter writer, PackageGraph packageGraph, Category category) {
+    TemplateData data = CategoryTemplateData(_options, packageGraph, category);
+    _write(writer, category.filePath, _templates.categoryTemplate, data);
+  }
+
+  @override
+  void generateLibrary(
+      FileWriter writer, PackageGraph packageGraph, Library lib) {
+    TemplateData data = LibraryTemplateData(_options, packageGraph, lib);
+    _write(writer, lib.filePath, _templates.libraryTemplate, data);
+  }
+
+  @override
+  void generateClass(
+      FileWriter writer, PackageGraph packageGraph, Library lib, Class clazz) {
+    TemplateData data = ClassTemplateData(_options, packageGraph, lib, clazz);
+    _write(writer, clazz.filePath, _templates.classTemplate, data);
+  }
+
+  @override
+  void generateExtension(FileWriter writer, PackageGraph packageGraph,
+      Library lib, Extension extension) {
+    TemplateData data =
+        ExtensionTemplateData(_options, packageGraph, lib, extension);
+    _write(writer, extension.filePath, _templates.extensionTemplate, data);
+  }
+
+  @override
+  void generateMixin(
+      FileWriter writer, PackageGraph packageGraph, Library lib, Mixin mixin) {
+    TemplateData data = MixinTemplateData(_options, packageGraph, lib, mixin);
+    _write(writer, mixin.filePath, _templates.mixinTemplate, data);
+  }
+
+  @override
+  void generateConstructor(FileWriter writer, PackageGraph packageGraph,
+      Library lib, Class clazz, Constructor constructor) {
+    TemplateData data = ConstructorTemplateData(
+        _options, packageGraph, lib, clazz, constructor);
+
+    _write(writer, constructor.filePath, _templates.constructorTemplate, data);
+  }
+
+  @override
+  void generateEnum(
+      FileWriter writer, PackageGraph packageGraph, Library lib, Enum eNum) {
+    TemplateData data = EnumTemplateData(_options, packageGraph, lib, eNum);
+
+    _write(writer, eNum.filePath, _templates.enumTemplate, data);
+  }
+
+  @override
+  void generateFunction(FileWriter writer, PackageGraph packageGraph,
+      Library lib, ModelFunction function) {
+    TemplateData data =
+        FunctionTemplateData(_options, packageGraph, lib, function);
+
+    _write(writer, function.filePath, _templates.functionTemplate, data);
+  }
+
+  @override
+  void generateMethod(FileWriter writer, PackageGraph packageGraph, Library lib,
+      Container clazz, Method method) {
+    TemplateData data =
+        MethodTemplateData(_options, packageGraph, lib, clazz, method);
+
+    _write(writer, method.filePath, _templates.methodTemplate, data);
+  }
+
+  @override
+  void generateConstant(FileWriter writer, PackageGraph packageGraph,
+          Library lib, Container clazz, Field property) =>
+      generateProperty(writer, packageGraph, lib, clazz, property);
+
+  @override
+  void generateProperty(FileWriter writer, PackageGraph packageGraph,
+      Library lib, Container clazz, Field property) {
+    TemplateData data =
+        PropertyTemplateData(_options, packageGraph, lib, clazz, property);
+
+    _write(writer, property.filePath, _templates.propertyTemplate, data);
+  }
+
+  @override
+  void generateTopLevelProperty(FileWriter writer, PackageGraph packageGraph,
+      Library lib, TopLevelVariable property) {
+    TemplateData data =
+        TopLevelPropertyTemplateData(_options, packageGraph, lib, property);
+
+    _write(
+        writer, property.filePath, _templates.topLevelPropertyTemplate, data);
+  }
+
+  @override
+  void generateTopLevelConstant(FileWriter writer, PackageGraph packageGraph,
+          Library lib, TopLevelVariable property) =>
+      generateTopLevelProperty(writer, packageGraph, lib, property);
+
+  @override
+  void generateTypeDef(FileWriter writer, PackageGraph packageGraph,
+      Library lib, Typedef typeDef) {
+    TemplateData data =
+        TypedefTemplateData(_options, packageGraph, lib, typeDef);
+
+    _write(writer, typeDef.filePath, _templates.typeDefTemplate, data);
+  }
+
+  @override
+  void generateAdditionalFiles(FileWriter writer, PackageGraph graph) async {
+    final prefix = 'package:dartdoc/resources/';
+    for (String resourcePath in resources.resource_names) {
+      if (!resourcePath.startsWith(prefix)) {
+        throw StateError('Resource paths must start with $prefix, '
+            'encountered $resourcePath');
+      }
+      String destFileName = resourcePath.substring(prefix.length);
+      writer.write(path.join('static-assets', destFileName),
+          await loader.loadAsBytes(resourcePath));
+    }
+  }
+}
