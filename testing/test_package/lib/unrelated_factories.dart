5fff9ebf5b0720c466cc80d1d1cf055b14a9507d
Janice Collins <jcollins@google.com>
2020-04-23 11:32:10 -0700

    First pass at repro attempt

---
+++ testing/test_package/lib/unrelated_factories.dart
@@ -0,0 +1,12 @@
+class A {
+  /// A link to [B.fromMap].
+  factory A.fromMap(Map<String, dynamic> map) => A();
+
+  A();
+}
+
+class B {
+  factory B.fromMap(Map<String, dynamic> map) => B();
+
+  B();
+}
