1276e33b3f17ef53177961883fa8a41e351980b9
Janice Collins <jcollins@google.com>
2020-10-01 08:05:00 -0700

    Switch coverage back to dev branch (#2367)

--- tool/travis.sh
+++ tool/travis.sh
@@ -11,9 +11,7 @@ set -ex
 export PATH="$PATH":"~/.pub-cache/bin"
 DART_VERSION=`dart --version 2>&1 | awk '{print $4}'`
 # Do not run coverage on non-dev builds or non-Linux platforms.
-# Disable coverage on dev builds and switch to stable until
-# dart-lang/sdk#43487 is fixed.
-if ! echo "${DART_VERSION}" | grep -q stable || ! uname | grep -q Linux ; then
+if ! echo "${DART_VERSION}" | grep -q dev || ! uname | grep -q Linux ; then
   unset COVERAGE_TOKEN
 fi
 
