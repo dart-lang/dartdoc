446b698aee7b6cc8f946f34c709d85b317c011ce
Parker Lougheed <parlough@gmail.com>
2021-01-05 17:47:50 -0600

    Remove URI library (#2471)
    * Remove URI library
    
    * Remove URI.js links
    
    * Add feature detection

--- lib/src/generator/html_resources.g.dart
+++ lib/src/generator/html_resources.g.dart
@@ -1,7 +1,6 @@
 // WARNING: This file is auto-generated. Do not taunt.
 
 const List<String> resource_names = [
-  'package:dartdoc/resources/URI.js',
   'package:dartdoc/resources/favicon.png',
   'package:dartdoc/resources/github.css',
   'package:dartdoc/resources/highlight.pack.js',
