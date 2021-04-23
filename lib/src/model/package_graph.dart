240137abac8a7de8065915305ba56ada19c044a0
Sam Rawlins <srawlins@google.com>
2021-04-23 13:03:56 -0700

    Fix directives sort ordering

--- lib/src/model/package_graph.dart
+++ lib/src/model/package_graph.dart
@@ -20,7 +20,6 @@ import 'package:dartdoc/src/render/renderer_factory.dart';
 import 'package:dartdoc/src/special_elements.dart';
 import 'package:dartdoc/src/tuple.dart';
 import 'package:dartdoc/src/warnings.dart';
-import 'package:dartdoc/src/model_utils.dart' show matchGlobs;
 
 class PackageGraph {
   PackageGraph.uninitialized(
@@ -976,7 +975,7 @@ class PackageGraph {
       // for nodoc's semantics.  Looking up the defining element just to pull
       // a context is again, slow.
       List<String> globs = config.optionSet['nodoc'].valueAt(file.parent2);
-      _configSetsNodocFor[fullName] = matchGlobs(globs, fullName);
+      _configSetsNodocFor[fullName] = utils.matchGlobs(globs, fullName);
     }
     return _configSetsNodocFor[fullName];
   }
