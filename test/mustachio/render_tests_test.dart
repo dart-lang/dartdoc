4cbfadbdae34b32a403a0fe8ad718d9ee5e0a35a
Sam Rawlins <srawlins@google.com>
2021-06-02 16:51:18 -0700

    Fix windows again

--- test/mustachio/render_tests_test.dart
+++ test/mustachio/render_tests_test.dart
@@ -15,8 +15,8 @@ void main() {
     var testCasePattern = RegExp(r'test\(.*,');
     var dartdocLibUri = await Isolate.resolvePackageUri(
         Uri.parse('package:dartdoc/dartdoc.dart'));
-    var dartdocPath =
-        p.dirname(p.dirname(p.joinAll(dartdocLibUri.pathSegments)));
+    var dartdocPath = p.dirname(
+        p.dirname(p.separator + p.joinAll(dartdocLibUri.pathSegments)));
     var runtimeRendererRenderTest = File(p.join(dartdocPath, 'test',
             'mustachio', 'runtime_renderer_render_test.dart'))
         .readAsLinesSync();
