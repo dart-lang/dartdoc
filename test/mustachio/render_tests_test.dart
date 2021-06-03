9edeedcaee06dcc81f6134f3b0f9e79bc4718576
Sam Rawlins <srawlins@google.com>
2021-06-03 10:01:21 -0700

    Fix Windows test

--- test/mustachio/render_tests_test.dart
+++ test/mustachio/render_tests_test.dart
@@ -17,7 +17,7 @@ void main() {
         Uri.parse('package:dartdoc/dartdoc.dart'));
     var dartdocPath = p.dirname(p.dirname(dartdocLibUri.path));
     // Correct Windows issue path coming out of [Isolate.resolvePackageUri].
-    if (dartdocPath.startsWith(p.windows.separator)) {
+    if (dartdocPath.startsWith('/')) {
       dartdocPath = dartdocPath.substring(1).replaceAll('/', p.separator);
     }
     var runtimeRendererRenderTest = File(p.join(dartdocPath, 'test',
