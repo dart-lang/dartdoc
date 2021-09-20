f704565d4e27c487ed325cf91517ef17a3a37293
Janice Collins <jcollins@google.com>
2021-09-20 15:15:17 -0700

    Remove overlapping imports; bleeding edge SDK warns on these now (#2798)

--- lib/src/model/inheritable.dart
+++ lib/src/model/inheritable.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:dartdoc/src/model/feature.dart';
-import 'package:dartdoc/src/model/inheriting_container.dart';
 import 'package:dartdoc/src/model/model.dart';
 import 'package:dartdoc/src/special_elements.dart';
 
