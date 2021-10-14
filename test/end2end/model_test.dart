73ac1d4c7582312202bdc9eae8f92427b0c5c116
Jonas Finnemann Jensen <jonasfj@google.com>
2021-10-14 12:40:52 +0200

    Fix number of classes

--- test/end2end/model_test.dart
+++ test/end2end/model_test.dart
@@ -2045,7 +2045,7 @@ void main() {
     });
 
     test('correctly finds all the classes', () {
-      expect(classes, hasLength(39));
+      expect(classes, hasLength(40));
     });
 
     test('abstract', () {
