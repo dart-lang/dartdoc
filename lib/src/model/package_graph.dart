00ff7902b60681dedd009eb77e9c433439f68011
Janice Collins <jcollins@google.com>
2021-10-21 12:47:40 -0700

    forgot to remove a fixme

--- lib/src/model/package_graph.dart
+++ lib/src/model/package_graph.dart
@@ -747,7 +747,6 @@ class PackageGraph with CommentReferable, Nameable, ModelBuilder {
   /// This doesn't know anything about [PackageGraph.inheritThrough] and probably
   /// shouldn't, so using it with [Inheritable]s without special casing is
   /// not advised.
-  // FIXME(nnbd): remove null check ignore in model_utils after migration
   ModelElement? findCanonicalModelElementFor(Element? e,
       {Container? preferredClass}) {
     assert(allLibrariesAdded);
