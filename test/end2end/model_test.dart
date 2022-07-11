a123c2cdd022c6de2c665e82814a636be78957a6
Sam Rawlins <srawlins@google.com>
2022-07-11 13:21:07 -0700

    37

--- test/end2end/model_test.dart
+++ test/end2end/model_test.dart
@@ -2041,7 +2041,7 @@ void main() {
     });
 
     test('correctly finds all the classes', () {
-      expect(classes, hasLength(38));
+      expect(classes, hasLength(37));
     });
 
     test('abstract', () {
