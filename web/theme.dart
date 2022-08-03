17ca1e1111f7c186bad1ef7d908ab8b76fb3184e
krl981 <karelys@google.com>
2022-08-03 22:22:00 +0000

    Format applied to theme.dart

--- web/theme.dart
+++ web/theme.dart
@@ -1,65 +1,58 @@
 import 'dart:html';
 
-
 void init() {
   var theme = document.getElementById('theme') as InputElement;
 
   var elementB = document.body;
 
   theme.addEventListener('change', (event) {
-    if(theme.checked == true) {
+    if (theme.checked == true) {
       if (theme.getAttribute('value') == 'light-theme') {
         elementB?.classes.remove('light-theme');
         elementB?.classes.toggle('dark-theme');
         theme.setAttribute('value', 'dark-theme');
-      }
-      else{
+      } else {
         elementB?.classes.remove('light-theme');
         elementB?.classes.toggle('dark-theme');
         theme.setAttribute('value', 'dark-theme');
       }
       window.localStorage['checked'] = 'true';
-    }
-    else{
+    } else {
       elementB?.classes.remove('dark-theme');
       elementB?.classes.toggle('light-theme');
       theme.setAttribute('value', 'light-theme');
       window.localStorage['checked'] = 'false';
-
     }
   });
 
-  void toggleTheme(){
-    if(theme.checked == true) {
+  void toggleTheme() {
+    if (theme.checked == true) {
       if (theme.getAttribute('value') == 'light-theme') {
         elementB?.classes.remove('light-theme');
         elementB?.classes.toggle('dark-theme');
         theme.setAttribute('value', 'dark-theme');
-      }
-      else{
+      } else {
         elementB?.classes.remove('light-theme');
         elementB?.classes.toggle('dark-theme');
         theme.setAttribute('value', 'dark-theme');
       }
       window.localStorage['checked'] = 'true';
-      theme.checked=true;
-    }
-    else{
+      theme.checked = true;
+    } else {
       elementB?.classes.remove('dark-theme');
       elementB?.classes.toggle('light-theme');
       theme.setAttribute('value', 'light-theme');
       window.localStorage['checked'] = 'false';
-      theme.checked=false;
+      theme.checked = false;
     }
   }
 
-  void setChecked(){
-    if(window.localStorage['checked']!=null){
-      if(window.localStorage['checked']=='true'){
-        theme.checked=true;
+  void setChecked() {
+    if (window.localStorage['checked'] != null) {
+      if (window.localStorage['checked'] == 'true') {
+        theme.checked = true;
         toggleTheme();
-      }
-      else {
+      } else {
         theme.checked = false;
         toggleTheme();
       }
