d3eaea627e5acadd278bd966ee644112a101a1b1
Karelys Lopez Rivera <106621169+klr981@users.noreply.github.com>
2022-08-10 04:29:12 +0000

    Dark Mode: Landed changes in this PR (#3113)

---
+++ web/theme.dart
@@ -0,0 +1,36 @@
+import 'dart:html';
+
+void init() {
+  var bodyElement = document.body;
+
+  if (bodyElement == null) {
+    return;
+  }
+
+  var theme = document.getElementById('theme') as InputElement;
+
+  theme.addEventListener('change', (event) {
+    if (theme.checked == true) {
+      bodyElement.setAttribute('class', 'dark-theme');
+      theme.setAttribute('value', 'dark-theme');
+      window.localStorage['colorTheme'] = 'true';
+    } else {
+      bodyElement.setAttribute('class', 'light-theme');
+      theme.setAttribute('value', 'light-theme');
+      window.localStorage['colorTheme'] = 'false';
+    }
+  });
+
+  if (window.localStorage['colorTheme'] != null) {
+    theme.checked = window.localStorage['colorTheme'] == 'true';
+    if (theme.checked == true) {
+      bodyElement.setAttribute('class', 'dark-theme');
+      theme.setAttribute('value', 'dark-theme');
+      window.localStorage['colorTheme'] = 'true';
+    } else {
+      bodyElement.setAttribute('class', 'light-theme');
+      theme.setAttribute('value', 'light-theme');
+      window.localStorage['colorTheme'] = 'false';
+    }
+  }
+}
