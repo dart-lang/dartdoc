f0faae98cd542f2bd94f39a8ebcbca9cdce28043
Sam Rawlins <srawlins@google.com>
2022-11-10 08:48:05 -0800

    re-add

---
+++ lib/src/generator/dartdoc_generator_backend.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+@Deprecated('Import generator_backend.dart directly')
+export 'generator_backend.dart';
+
