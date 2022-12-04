f7d62603c0a19f29fcc311db43e5d7a97a30e51b
Parker Lougheed <parlough@gmail.com>
2022-12-04 12:20:58 -0600

    Remove unused import

--- lib/src/generator/template_data.dart
+++ lib/src/generator/template_data.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/dart/element/element.dart';
 import 'package:dartdoc/src/model/model.dart';
 
 typedef ContainerSidebar = String Function(
