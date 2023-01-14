31e9c7977a2f65ea87b08750b6584477cb65e235
Parker Lougheed <parlough@gmail.com>
2023-01-14 10:31:33 -0600

    Fix build by removing test opting out of null safety (#3297)
    * Remove test opting out of null safety
    
    * Account for removed library

--- test/end2end/model_test.dart
+++ test/end2end/model_test.dart
@@ -270,7 +270,6 @@ void main() {
   group('NNBD cases', () {
     late final Library lateFinalWithoutInitializer,
         nullSafetyClassMemberDeclarations,
-        optOutOfNullSafety,
         nullableElements;
     late final Class b;
     late final Class c;
@@ -282,8 +281,6 @@ void main() {
           .firstWhere((lib) => lib.name == 'late_final_without_initializer');
       nullSafetyClassMemberDeclarations = packageGraph.libraries
           .firstWhere((lib) => lib.name == 'nnbd_class_member_declarations');
-      optOutOfNullSafety = packageGraph.libraries
-          .firstWhere((lib) => lib.name == 'opt_out_of_nnbd');
       nullableElements = packageGraph.libraries
           .firstWhere((lib) => lib.name == 'nullable_elements');
       b = nullSafetyClassMemberDeclarations.allClasses
@@ -319,7 +316,6 @@ void main() {
 
     test('isNullSafety is set correctly for libraries', () {
       expect(lateFinalWithoutInitializer.isNullSafety, isTrue);
-      expect(optOutOfNullSafety.isNullSafety, isFalse);
     });
 
     test('method parameters with required', () {
@@ -415,13 +411,6 @@ void main() {
       expect(initializeMe.isLate, isTrue);
       expect(initializeMe.features, contains(Feature.lateFeature));
       expect(initializeMe.features, isNot(contains(Feature.readWrite)));
-    });
-
-    test('Opt out of Null safety', () {
-      var notOptedIn = optOutOfNullSafety.publicProperties
-          .firstWhere((v) => v.name == 'notOptedIn');
-      expect(notOptedIn.isNullSafety, isFalse);
-      expect(notOptedIn.modelType.nullabilitySuffix, isEmpty);
     });
 
     test('complex nullable elements are detected and rendered correctly', () {
@@ -888,12 +877,6 @@ void main() {
 
     test('has a name', () {
       expect(exLibrary.name, 'ex');
-    });
-
-    test('does not have a null safety label if not null safe', () {
-      var optOutLibrary = packageGraph.libraries
-          .firstWhere((lib) => lib.name == 'opt_out_of_nnbd');
-      expect(optOutLibrary.isNullSafety, isFalse);
     });
 
     test('has a line number and column', () {
