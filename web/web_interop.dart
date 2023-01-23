26b331a83855210d9c62e1184d91f4d462aace9f
Parker Lougheed <parlough@gmail.com>
2023-01-23 15:18:53 -0600

    Switch import to dart:js_util and add copyright headers

--- web/web_interop.dart
+++ web/web_interop.dart
@@ -1,5 +1,10 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:js_util' as js_util;
+
 import 'package:js/js.dart';
-import 'package:js/js_util.dart' as js_util;
 
 @JS('Response')
 @staticInterop
