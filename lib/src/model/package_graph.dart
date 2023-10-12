a212e999035d2c3d8b0bfe223ebb298d8179dadf
Sam Rawlins <srawlins@google.com>
2023-10-12 07:34:44 -0700

    Remove cross-export warning.

--- lib/src/model/package_graph.dart
+++ lib/src/model/package_graph.dart
@@ -418,7 +418,6 @@ class PackageGraph with CommentReferable, Nameable, ModelBuilder {
       PackageWarning.ambiguousDocReference ||
       PackageWarning.ignoredCanonicalFor ||
       PackageWarning.packageOrderGivesMissingPackageName ||
-      PackageWarning.reexportedPrivateApiAcrossPackages ||
       PackageWarning.unresolvedDocReference ||
       PackageWarning.unknownMacro ||
       PackageWarning.unknownHtmlFragment ||
