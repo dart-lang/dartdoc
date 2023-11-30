0499c4e4abeddd8994ddfa33fe6de287ac2e8b1d
Kallen Tu <kallentu@google.com>
2023-11-29 16:04:05 -0800

    Deprecate public exports in lib/dartdoc.dart. (#3589)

--- test/end2end/dartdoc_test.dart
+++ test/end2end/dartdoc_test.dart
@@ -7,8 +7,8 @@ library dartdoc.dartdoc_test;
 import 'dart:async';
 
 import 'package:analyzer/file_system/file_system.dart';
-import 'package:dartdoc/dartdoc.dart' show Dartdoc, DartdocResults;
 import 'package:dartdoc/options.dart';
+import 'package:dartdoc/src/dartdoc.dart' show Dartdoc, DartdocResults;
 import 'package:dartdoc/src/dartdoc_options.dart';
 import 'package:dartdoc/src/failure.dart';
 import 'package:dartdoc/src/io_utils.dart';
