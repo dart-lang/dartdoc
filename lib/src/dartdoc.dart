95dd571cbbec021cb96a177e19e46233f3ca3739
Sam Rawlins <srawlins@google.com>
2024-02-28 13:21:24 -0800

    Deprecate the 'include-externals' option.
    I cannot see that this is used anywhere. We also have no tests for it. The
    documentation is minimal. It was added 8 years ago! How time flies!
    
    I suspect this was put in place for Flutter, and Flutter switched to using
    the newer `--auto-include-dependencies` option.

--- lib/src/dartdoc.dart
+++ lib/src/dartdoc.dart
@@ -188,6 +188,14 @@ class Dartdoc {
     runtimeStats.startPerfTask('buildPackageGraph');
     var packageGraph = await packageBuilder.buildPackageGraph();
     runtimeStats.endPerfTask();
+    if (packageBuilder.includeExternalsWasSpecified) {
+      packageGraph.defaultPackage.warn(
+        PackageWarning.deprecated,
+        message:
+            "The '--include-externals' option is deprecated, and will soon be "
+            'removed.',
+      );
+    }
     var libs = packageGraph.libraryCount;
     logInfo("Initialized dartdoc with $libs librar${libs == 1 ? 'y' : 'ies'}");
 
