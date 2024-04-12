4c6adc43e23b0e8db6404aabcb710600e714b772
Sam Rawlins <srawlins@google.com>
2024-04-11 22:04:44 -0700

    import

--- test/library_test.dart
+++ test/library_test.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/file_system/memory_file_system.dart';
-import 'package:collection/collection.dart';
 import 'package:dartdoc/src/model/model.dart';
 import 'package:test/test.dart';
 
