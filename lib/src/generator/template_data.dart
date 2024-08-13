38931f5e8cace0c71fea4406db10330eed8eb18e
Sam Rawlins <srawlins@google.com>
2024-08-13 09:52:41 -0700

    Add a doc-import into template_data.dart to resolve 'GeneratorBackend.write'

--- lib/src/generator/template_data.dart
+++ lib/src/generator/template_data.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// @docImport 'generator_backend.dart';
+library;
+
 import 'package:dartdoc/src/model/model.dart';
 
 typedef ContainerSidebar = String Function(
