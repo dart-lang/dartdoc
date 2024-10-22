db81d941846f612b16d7779a7d5b3a9ba1c721a7
Sam Rawlins <srawlins@google.com>
2024-10-22 14:30:44 -0700

    Move ToolConfiguration wholesale to it's own file (#3911)

--- test/tool_runner_test.dart
+++ test/tool_runner_test.dart
@@ -4,8 +4,8 @@
 
 import 'dart:io';
 
-import 'package:dartdoc/src/dartdoc_options.dart';
 import 'package:dartdoc/src/package_meta.dart';
+import 'package:dartdoc/src/tool_configuration.dart';
 import 'package:dartdoc/src/tool_definition.dart';
 import 'package:dartdoc/src/tool_runner.dart';
 import 'package:path/path.dart' as path;
