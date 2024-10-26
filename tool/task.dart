d4876f9492a9ebe31e66183ca8504d97af5be916
Parker Lougheed <parlough@gmail.com>
2024-10-25 22:25:12 -0500

    Compress generated css by configuring sass output style (#3919)

--- tool/task.dart
+++ tool/task.dart
@@ -178,9 +178,11 @@ Future<void> buildWeb({bool debug = false}) async {
   ]);
   _delete(File('lib/resources/docs.dart.js.deps'));
 
-  final compileResult = sass.compileToResult('web/styles/styles.scss');
+  final compileResult = sass.compileToResult('web/styles/styles.scss',
+      style: debug ? sass.OutputStyle.expanded : sass.OutputStyle.compressed);
   if (compileResult.css.isNotEmpty) {
-    File('lib/resources/styles.css').writeAsStringSync(compileResult.css);
+    File('lib/resources/styles.css')
+        .writeAsStringSync('${compileResult.css}\n');
   } else {
     throw StateError('Compiled CSS was empty.');
   }
