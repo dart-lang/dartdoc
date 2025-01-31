f0d2276e57c96717e625964ea0385a30f2237e62
Keerti Parthasarathy <keertip@users.noreply.github.com>
2025-01-31 10:10:51 -0800

    Elements. Migrate to element2 (#3978)

--- lib/src/model/source_code_mixin.dart
+++ lib/src/model/source_code_mixin.dart
@@ -2,9 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: analyzer_use_new_elements
-
-import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/source/line_info.dart';
 import 'package:dartdoc/src/model/model.dart';
 
@@ -12,8 +9,6 @@ mixin SourceCode implements Documentable {
   ModelNode? get modelNode;
 
   CharacterLocation? get characterLocation;
-
-  Element? get element;
 
   bool get hasSourceCode => config.includeSource && sourceCode.isNotEmpty;
 
