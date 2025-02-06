7c9dcd85969ada5186f1c3ee07c9408ead759470
Keerti Parthasarathy <keertip@google.com>
2025-02-06 11:25:36 -0800

    Fix test failure

--- test/end2end/model_test.dart
+++ test/end2end/model_test.dart
@@ -4,6 +4,8 @@
 
 // ignore_for_file: non_constant_identifier_names
 
+ // ignore_for_file: analyzer_use_new_elements
+
 import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/source/line_info.dart';
