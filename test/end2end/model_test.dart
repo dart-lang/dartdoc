7ab502ce67ca90cec1376ffc0b63379eb022e207
Sam Rawlins <srawlins@google.com>
2025-02-14 14:56:11 -0800

    put-it-back

--- test/end2end/model_test.dart
+++ test/end2end/model_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+ // ignore_for_file: analyzer_use_new_elements
+
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:analyzer/dart/element/element2.dart';
