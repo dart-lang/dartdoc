769e09e1852d89bea7fac3a22400e638dc4cccbf
sgrekhov <sgrekhov22@gmail.com>
2026-01-27 10:45:55 +0200

    #4134. Add placement tests for private top-level declarations

--- test/co19/placement_test.dart
+++ test/co19/placement_test.dart
@@ -68,7 +68,7 @@ class C { /// Line 2
     expect(c.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a class.
+  /// Check that block-based doc comments can be placed before a class.
   void test_beforeClass2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -81,6 +81,35 @@ class C { /** Line 2 */
  */
 ''');
     var c = library.classes.named('C');
+    expect(c.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private class.
+  void test_beforeClass3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+class _C { /// Line 2
+/// Line 3
+}
+/// Line 4
+''');
+    var c = library.classes.named('_C');
+    expect(c.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private class.
+  void test_beforeClass4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+class _C { /** Line 2 */
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var c = library.classes.named('_C');
     expect(c.documentation, 'Line 1');
   }
 
@@ -97,7 +126,7 @@ mixin M { /// Line 2
     expect(m.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a mixin.
+  /// Check that block-based doc comments can be placed before a mixin.
   void test_beforeMixin2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -110,6 +139,35 @@ mixin M { /** Line 2 */
  */
 ''');
     var m = library.mixins.named('M');
+    expect(m.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private mixin.
+  void test_beforeMixin3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+mixin _M { /// Line 2
+/// Line 3
+}
+/// Line 4
+''');
+    var m = library.mixins.named('_M');
+    expect(m.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private mixin.
+  void test_beforeMixin4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+mixin _M { /** Line 2 */
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var m = library.mixins.named('_M');
     expect(m.documentation, 'Line 1');
   }
 
@@ -127,7 +185,7 @@ enum E { /// Line 2
     expect(e.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before an enum.
+  /// Check that block-based doc comments can be placed before an enum.
   void test_beforeEnum2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -141,6 +199,37 @@ enum E { /** Line 2 */
  */
 ''');
     var e = library.enums.named('E');
+    expect(e.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private enum.
+  void test_beforeEnum3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+enum _E { /// Line 2
+  e0;
+/// Line 3
+}
+/// Line 4
+''');
+    var e = library.enums.named('_E');
+    expect(e.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private enum.
+  void test_beforeEnum4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+enum _E { /** Line 2 */
+  e0;
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var e = library.enums.named('_E');
     expect(e.documentation, 'Line 1');
   }
 
@@ -159,7 +248,7 @@ extension Ext { /// Line 2
     expect(ext.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before an extension.
+  /// Check that block-based doc comments can be placed before an extension.
   void test_beforeExtension2() async {
     var library = await bootPackageWithLibrary('''
 class A {}
@@ -177,6 +266,40 @@ extension Ext { /** Line 2 */
     expect(ext.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private extension.
+  void test_beforeExtension3() async {
+    var library = await bootPackageWithLibrary('''
+class A {}
+    
+/// Line 1
+extension _Ext { /// Line 2
+/// Line 3
+}
+/// Line 4
+''');
+    var ext = library.extensions.named('_Ext');
+    expect(ext.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private
+  /// extension.
+  void test_beforeExtension4() async {
+    var library = await bootPackageWithLibrary('''
+class A {}
+    
+/** Line 1
+ */
+extension _Ext { /** Line 2 */
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var ext = library.extensions.named('_Ext');
+    expect(ext.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before an extension type.
   void test_beforeExtensionType1() async {
     var library = await bootPackageWithLibrary('''
@@ -190,7 +313,7 @@ extension type ET(int _) { /// Line 2
     expect(et.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before an extension type.
+  /// Check that block-based doc comments can be placed before an extension type.
   void test_beforeExtensionType2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -206,6 +329,36 @@ extension type ET(int _) { /** Line 2 */
     expect(et.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private extension type.
+  void test_beforeExtensionType3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+extension type _ET(int _) { /// Line 2
+/// Line 3
+}
+/// Line 4
+''');
+    var et = library.extensionTypes.named('_ET');
+    expect(et.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private
+  /// extension type.
+  void test_beforeExtensionType4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+extension type _ET(int _) { /** Line 2 */
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var et = library.extensionTypes.named('_ET');
+    expect(et.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before a typedef.
   void test_beforeTypedef1() async {
     var library = await bootPackageWithLibrary('''
@@ -217,7 +370,7 @@ typedef IntAlias = int; /// Line 2
     expect(td.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a typedef.
+  /// Check that block-based doc comments can be placed before a typedef.
   void test_beforeTypedef2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -230,8 +383,32 @@ typedef IntAlias = int; /** Line 2 */
     expect(td.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a typedef.
+  /// Check that doc comments can be placed before a private typedef.
   void test_beforeTypedef3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+typedef _IntAlias = int; /// Line 2
+/// Line 3
+''');
+    var td = library.typedefs.named('_IntAlias');
+    expect(td.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private typedef.
+  void test_beforeTypedef4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+typedef _IntAlias = int; /** Line 2 */
+/** Line 3
+ */
+''');
+    var td = library.typedefs.named('_IntAlias');
+    expect(td.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before an old-fashioned typedef.
+  void test_beforeTypedef5() async {
     var library = await bootPackageWithLibrary('''
 /// Line 1
 typedef void Foo(); /// Line 2
@@ -241,8 +418,9 @@ typedef void Foo(); /// Line 2
     expect(td.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a typedef.
-  void test_beforeTypedef4() async {
+  /// Check that block-based doc comments can be placed before an old-fashioned
+  /// typedef.
+  void test_beforeTypedef6() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
  */
@@ -251,6 +429,32 @@ typedef void Foo(); /** Line 2 */
  */
 ''');
     var td = library.typedefs.named('Foo');
+    expect(td.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before an old-fashioned private
+  /// typedef.
+  void test_beforeTypedef7() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+typedef void _Foo(); /// Line 2
+/// Line 3
+''');
+    var td = library.typedefs.named('_Foo');
+    expect(td.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before an old-fashioned
+  /// private typedef.
+  void test_beforeTypedef8() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+typedef void _Foo(); /** Line 2 */
+/** Line 3
+ */
+''');
+    var td = library.typedefs.named('_Foo');
     expect(td.documentation, 'Line 1');
   }
 
@@ -267,7 +471,8 @@ void foo() { /// Line 2
     expect(f.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a top-level function.
+  /// Check that block-based doc comments can be placed before a top-level
+  /// function.
   void test_beforeFunction2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -283,6 +488,36 @@ void foo() { /** Line 2 */
     expect(f.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private top-level function.
+  void test_beforeFunction3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+void _foo() { /// Line 2
+/// Line 3
+}
+/// Line 4
+''');
+    var f = library.functions.named('_foo');
+    expect(f.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private
+  /// top-level function.
+  void test_beforeFunction4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+void _foo() { /** Line 2 */
+/** Line 3
+ */
+}
+/** Line 4
+ */
+''');
+    var f = library.functions.named('_foo');
+    expect(f.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before a variable.
   void test_beforeVariable1() async {
     var library = await bootPackageWithLibrary('''
@@ -294,7 +529,7 @@ int x = 0; /// Line 2
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a variable.
+  /// Check that block-based doc comments can be placed before a variable.
   void test_beforeVariable2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -307,8 +542,33 @@ int x = 0; /** Line 2 */
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a final variable.
+  /// Check that doc comments can be placed before a private variable.
   void test_beforeVariable3() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+int _x = 0; /// Line 2
+/// Line 3
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private
+  /// variable.
+  void test_beforeVariable4() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+int _x = 0; /** Line 2 */
+/** Line 3
+ */
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a final variable.
+  void test_beforeVariable5() async {
     var library = await bootPackageWithLibrary('''
 /// Line 1
 final int x = 0; /// Line 2
@@ -318,8 +578,8 @@ final int x = 0; /// Line 2
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a variable.
-  void test_beforeVariable4() async {
+  /// Check that block-based doc comments can be placed before a final variable.
+  void test_beforeVariable6() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
  */
@@ -331,8 +591,33 @@ final int x = 0; /** Line 2 */
     expect(v.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private final variable.
+  void test_beforeVariable7() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+final int _x = 0; /// Line 2
+/// Line 3
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private final
+  /// variable.
+  void test_beforeVariable8() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+final int _x = 0; /** Line 2 */
+/** Line 3
+ */
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before a late variable.
-  void test_beforeVariable5() async {
+  void test_beforeVariable9() async {
     var library = await bootPackageWithLibrary('''
 /// Line 1
 late int x; /// Line 2
@@ -342,8 +627,8 @@ late int x; /// Line 2
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a variable.
-  void test_beforeVariable6() async {
+  /// Check that block-based doc comments can be placed before a late variable.
+  void test_beforeVariable10() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
  */
@@ -355,8 +640,33 @@ late int x = 0; /** Line 2 */
     expect(v.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private late variable.
+  void test_beforeVariable11() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+late int _x; /// Line 2
+/// Line 3
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private late
+  /// variable.
+  void test_beforeVariable12() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+late int _x = 0; /** Line 2 */
+/** Line 3
+ */
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before a late final variable.
-  void test_beforeVariable7() async {
+  void test_beforeVariable13() async {
     var library = await bootPackageWithLibrary('''
 /// Line 1
 late final int x; /// Line 2
@@ -366,8 +676,9 @@ late final int x; /// Line 2
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a late final variable.
-  void test_beforeVariable8() async {
+  /// Check that block-based doc comments can be placed before a late final
+  /// variable.
+  void test_beforeVariable14() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
  */
@@ -379,8 +690,33 @@ late final int x = 0; /** Line 2 */
     expect(v.documentation, 'Line 1');
   }
 
+  /// Check that doc comments can be placed before a private late final variable.
+  void test_beforeVariable15() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+late final int _x; /// Line 2
+/// Line 3
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private late
+  /// final variable.
+  void test_beforeVariable16() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+late final int _x = 0; /** Line 2 */
+/** Line 3
+ */
+''');
+    var v = library.properties.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
   /// Check that doc comments can be placed before a constant.
-  void test_beforeVariable9() async {
+  void test_beforeVariable17() async {
     var library = await bootPackageWithLibrary('''
 /// Line 1
 const x = 42; /// Line 2
@@ -390,8 +726,8 @@ const x = 42; /// Line 2
     expect(v.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a constant.
-  void test_beforeVariable10() async {
+  /// Check that block-based doc comments can be placed before a constant.
+  void test_beforeVariable18() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
  */
@@ -400,6 +736,31 @@ const x = 0; /** Line 2 */
  */
 ''');
     var v = library.constants.named('x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private constant.
+  void test_beforeVariable19() async {
+    var library = await bootPackageWithLibrary('''
+/// Line 1
+const _x = 42; /// Line 2
+/// Line 3
+''');
+    var v = library.constants.named('_x');
+    expect(v.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private
+  /// constant.
+  void test_beforeVariable20() async {
+    var library = await bootPackageWithLibrary('''
+/** Line 1
+ */
+const _x = 0; /** Line 2 */
+/** Line 3
+ */
+''');
+    var v = library.constants.named('_x');
     expect(v.documentation, 'Line 1');
   }
 
@@ -414,7 +775,7 @@ int get g => 0; /// Line 2
     expect(g.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a getter.
+  /// Check that block based doc comments can be placed before a getter.
   void test_beforeGetter2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -425,6 +786,34 @@ int get g => 0; /** Line 2 */
 ''');
     var g = library.properties.named('g');
     expect(g.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private getter.
+  void test_beforeGetter3() async {
+    markTestSkipped('Skipping until issue '
+        'https://github.com/dart-lang/dartdoc/issues/4185 is resolved.');
+//     var library = await bootPackageWithLibrary('''
+// /// Line 1
+// int get _g => 0; /// Line 2
+// /// Line 3
+// ''');
+//     var g = library.properties.named('_g');
+//     expect(g.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private getter.
+  void test_beforeGetter4() async {
+    markTestSkipped('Skipping until issue '
+        'https://github.com/dart-lang/dartdoc/issues/4185 is resolved.');
+//     var library = await bootPackageWithLibrary('''
+// /** Line 1
+//  */
+// int get _g => 0; /** Line 2 */
+// /** Line 3
+//  */
+// ''');
+//     var g = library.properties.named('_g');
+//     expect(g.documentation, 'Line 1');
   }
 
   /// Check that doc comments can be placed before a setter.
@@ -440,7 +829,7 @@ void set s(int _) { /// Line 2
     expect(s.documentation, 'Line 1');
   }
 
-  /// Check that doc comments can be placed before a setter.
+  /// Check that block-based doc comments can be placed before a setter.
   void test_beforeSetter2() async {
     var library = await bootPackageWithLibrary('''
 /** Line 1
@@ -454,5 +843,38 @@ void set s(int _) { /** Line 2 */
 ''');
     var s = library.properties.named('s');
     expect(s.documentation, 'Line 1');
+  }
+
+  /// Check that doc comments can be placed before a private setter.
+  void test_beforeSetter3() async {
+    markTestSkipped('Skipping until issue '
+        'https://github.com/dart-lang/dartdoc/issues/4185 is resolved.');
+//     var library = await bootPackageWithLibrary('''
+// /// Line 1
+// void set _s(int _) { /// Line 2
+// /// Line 3
+// }
+// /// Line 4
+// ''');
+//     var s = library.properties.named('_s');
+//     expect(s.documentation, 'Line 1');
+  }
+
+  /// Check that block-based doc comments can be placed before a private setter.
+  void test_beforeSetter4() async {
+    markTestSkipped('Skipping until issue '
+        'https://github.com/dart-lang/dartdoc/issues/4185 is resolved.');
+//     var library = await bootPackageWithLibrary('''
+// /** Line 1
+//  */
+// void set _s(int _) { /** Line 2 */
+// /** Line 3
+//  */
+// }
+// /** Line 4
+//  */
+// ''');
+//     var s = library.properties.named('_s');
+//     expect(s.documentation, 'Line 1');
   }
 }
