a08c9ef9d6bde8e2257f2cbb42d3a605ebe5731a
Sarah Zakarias <zarah@google.com>
2026-02-11 13:44:03 +0000

    Add support for the example directive

---
+++ test/example_directive_test.dart
@@ -0,0 +1,161 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:dartdoc/src/warnings.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import 'documentation_comment_test_base.dart';
+import 'src/utils.dart' as utils;
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(ExampleDirectiveTest);
+  });
+}
+
+@reflectiveTest
+class ExampleDirectiveTest extends DocumentationCommentTestBase {
+  Future<void> _bootPackage(String comment,
+      {Map<String, String> files = const {},
+      String libraryPath = 'lib/a.dart'}) async {
+    projectRoot = utils.writePackage(packageName, resourceProvider);
+    for (var entry in files.entries) {
+      var pathParts = entry.key.split('/');
+      var currentFolder = projectRoot;
+      for (var i = 0; i < pathParts.length - 1; i++) {
+        currentFolder = currentFolder.getChildAssumingFolder(pathParts[i]);
+      }
+      currentFolder
+          .getChildAssumingFile(pathParts.last)
+          .writeAsStringSync(entry.value);
+    }
+
+    var pathParts = libraryPath.split('/');
+    var currentFolder = projectRoot;
+    for (var i = 0; i < pathParts.length - 1; i++) {
+      currentFolder = currentFolder.getChildAssumingFolder(pathParts[i]);
+    }
+    currentFolder
+        .getChildAssumingFile(pathParts.last)
+        .writeAsStringSync('$comment\nlibrary;');
+
+    packageGraph =
+        await utils.bootBasicPackage(projectRoot.path, packageMetaProvider);
+    libraryModel = packageGraph.defaultPackage.libraries
+        .firstWhere((l) => l.sourceFileName.endsWith(libraryPath));
+  }
+
+  void test_processesExampleDirective() async {
+    await _bootPackage('''
+/// Text.
+///
+/// {@example /examples/hello.dart}
+///
+/// End text.
+''', files: {
+      'examples/hello.dart': 'void main() => print("hello");',
+    });
+
+    var doc = await libraryModel.processComment();
+
+    expectNoWarnings();
+    expect(doc, equals('''
+Text.
+
+
+```dart
+void main() => print("hello");
+```
+
+
+End text.'''));
+  }
+
+  void test_processesExampleDirective_withLang() async {
+    await _bootPackage('''
+/// {@example /examples/hello.txt lang=text}
+''', files: {
+      'examples/hello.txt': 'hello world',
+    });
+
+    var doc = await libraryModel.processComment();
+
+    expectNoWarnings();
+    expect(doc, equals('''
+
+```text
+hello world
+```
+'''));
+  }
+
+  void test_processesExampleDirective_withIndentKeep() async {
+    await _bootPackage('''
+/// {@example /examples/hello.dart indent=keep}
+''', files: {
+      'examples/hello.dart': '  void main() {\n    print("hello");\n  }',
+    });
+
+    var doc = await libraryModel.processComment();
+
+    expectNoWarnings();
+    expect(doc, equals('''
+
+```dart
+  void main() {
+    print("hello");
+  }
+```
+'''));
+  }
+
+  void test_processesExampleDirective_withIndentStrip() async {
+    await _bootPackage('''
+/// {@example /examples/hello.dart indent=strip}
+''', files: {
+      'examples/hello.dart': '  void main() {\n    print("hello");\n  }',
+    });
+
+    var doc = await libraryModel.processComment();
+
+    expectNoWarnings();
+    expect(doc, equals('''
+
+```dart
+void main() {
+  print("hello");
+}
+```
+'''));
+  }
+
+  void test_exampleDirective_missingFile() async {
+    await _bootPackage('''
+/// {@example /examples/non_existent.dart}
+''');
+
+    await libraryModel.processComment();
+
+    expect(
+      libraryModel,
+      hasWarning(
+          PackageWarning.missingExampleFile, '/examples/non_existent.dart'),
+    );
+  }
+
+  void test_exampleDirective_missingFilePath() async {
+    await _bootPackage('''
+/// {@example}
+''');
+
+    await libraryModel.processComment();
+
+    expect(
+      libraryModel,
+      hasWarning(PackageWarning.invalidParameter,
+          'Must specify a file path for the @example directive.'),
+    );
+  }
+}
---
+++ test/example_path_resolution_test.dart
@@ -0,0 +1,154 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:dartdoc/src/model/documentation_comment.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+void main() {
+  group('DocumentationComment.resolveExamplePath', () {
+    late p.Context pathContext;
+    late String packagePath;
+
+    setUp(() {
+      pathContext = p.Context(style: p.Style.posix, current: '/project');
+      packagePath = '/project';
+    });
+
+    String? resolve(String filepath, {String? sourceFileName}) {
+      return DocumentationComment.resolveExamplePath(
+        filepath,
+        packagePath: packagePath,
+        sourceFileName: sourceFileName,
+        pathContext: pathContext,
+        warn: (kind, {message}) {},
+      )?.path;
+    }
+
+    test('absolute path from root', () {
+      expect(resolve('/examples/hello.dart'), '/project/examples/hello.dart');
+    });
+
+    test('relative path from root library', () {
+      expect(
+        resolve('examples/hello.dart', sourceFileName: '/project/lib/a.dart'),
+        '/project/lib/examples/hello.dart',
+      );
+    });
+
+    test('relative path with ..', () {
+      expect(
+        resolve('../examples/hello.dart',
+            sourceFileName: '/project/lib/a.dart'),
+        '/project/examples/hello.dart',
+      );
+    });
+
+    test('relative path from nested library', () {
+      expect(
+        resolve('util.dart', sourceFileName: '/project/lib/src/a.dart'),
+        '/project/lib/src/util.dart',
+      );
+    });
+
+    test('relative path with null sourceFileName (resolved from root)', () {
+      expect(
+        resolve('examples/hello.dart', sourceFileName: null),
+        '/project/examples/hello.dart',
+      );
+    });
+
+    test('path with URI encoded characters', () {
+      expect(
+        resolve('/examples/hello%20world.dart'),
+        '/project/examples/hello world.dart',
+      );
+    });
+
+    test('path with fragment and query (ignored)', () {
+      expect(
+        resolve('/examples/hello.dart?q=1#region'),
+        '/project/examples/hello.dart',
+      );
+    });
+
+    test('clamping: relative traversal cannot escape root', () {
+      expect(
+        resolve('../../../examples/hello.dart',
+            sourceFileName: '/project/lib/a.dart'),
+        '/project/examples/hello.dart',
+      );
+    });
+
+    test('clamping: absolute traversal cannot escape root', () {
+      expect(
+        resolve('/../outside.dart'),
+        '/project/outside.dart',
+      );
+    });
+
+    test('absolute URI (resolves its path relative to root)', () {
+      expect(
+        resolve('https://example.com/hello.dart'),
+        '/project/hello.dart',
+      );
+    });
+
+    test('path with backslashes', () {
+      // Uri.resolve converts backslashes to forward slashes.
+      var result = resolve('examples\\hello.dart',
+          sourceFileName: '/project/lib/a.dart');
+      expect(result, '/project/lib/examples/hello.dart');
+    });
+
+    test('unusual path inputs (no crash)', () {
+      var inputs = [
+        '\x00',
+        ' ',
+        '%%',
+        '',
+        '..',
+        '//',
+        '\\..\\..\\',
+        'C:\\Windows\\System32',
+        'http://[::1]/',
+        'a' * 10000,
+      ];
+
+      for (var input in inputs) {
+        try {
+          DocumentationComment.resolveExamplePath(
+            input,
+            packagePath: packagePath,
+            sourceFileName: '/project/lib/a.dart',
+            pathContext: pathContext,
+            warn: (kind, {message}) {},
+          );
+        } catch (e) {
+          fail('resolveExamplePath crashed on input "$input": $e');
+        }
+      }
+    });
+
+    test('invalid URI inputs (trigger warnings)', () {
+      var inputs = [
+        ': [', // Invalid URI
+        'http://[::1]]', // Invalid IPv6
+        '/%ff%fe%fd', // Invalid UTF-8 encoding
+      ];
+
+      for (var input in inputs) {
+        var warned = false;
+        DocumentationComment.resolveExamplePath(
+          input,
+          packagePath: packagePath,
+          sourceFileName: '/project/lib/a.dart',
+          pathContext: pathContext,
+          warn: (kind, {message}) => warned = true,
+        );
+        expect(warned, isTrue, reason: 'Expected a warning for input "$input"');
+      }
+    });
+  });
+}
