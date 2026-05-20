2b32137dad83c3efb5322cdd7acfe18bc5616ac9
Sarah Zakarias <zarah@google.com>
2026-05-20 13:22:57 +0000

    Add baseline tests for Primary Constructors field induction and metadata propagation

--- test/dartdoc_test_base.dart
+++ test/dartdoc_test_base.dart
@@ -47,7 +47,11 @@ abstract class DartdocTestBase {
   /// These are written out to a pubspec file using `path` keys.
   Map<String, String> get pubDependencyPaths => const {};
 
-  List<String> get experiments => ['enhanced-parts', 'wildcard-variables'];
+  List<String> get experiments => [
+        'enhanced-parts',
+        'wildcard-variables',
+        'primary-constructors',
+      ];
 
   bool get skipUnreachableSdkLibraries => true;
 
